0c12609897287de675d1ae31468ad53f5da24dbf
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-11 18:09:19 -0500

    append txt file

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,7 @@
 const fs = require('fs');
 
-fs.writeFileSync('notes.txt', 'Hey man! I just made this file with nodejs.');
+// fs.writeFileSync('notes.txt', 'Hey man! I just made this file with nodejs.');
+fs.appendFile('notes.txt', 'Im appending this data.', err => {
+  if (err) console.log(err); 
+  console.log('worked');
+});
