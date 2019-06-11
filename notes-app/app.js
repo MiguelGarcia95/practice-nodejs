1780e7d1b57bdd558079a538d96bac7f2e64f6c8
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-11 18:31:13 -0500

    import files

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,3 @@
-const fs = require('fs');
+require('./utils.js');
 
-// fs.writeFileSync('notes.txt', 'Hey man! I just made this file with nodejs.');
-fs.appendFile('notes.txt', ' Im appending this data.', err => {
-  if (err) console.log(err); 
-  console.log('worked');
-});
+console.log(name);
