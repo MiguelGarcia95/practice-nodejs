74a240356b85a7adc78f83a46e79acf31e185942
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-11 19:30:13 -0500

    validate strings

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,8 @@
-const notes = require('./notes');
+const validator = require('validator');
 
+const notes = require('./notes');
 const note = notes();
 
 console.log(note);
+
+console.log(validator.isEmail('miguel@email.com'));
