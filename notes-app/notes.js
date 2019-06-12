e0fedd3e2a8f57e42851e5e1c2796d8ed7e82f83
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-11 22:10:56 -0500

    create createNote faction

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -2,4 +2,9 @@ const getNotes = () => {
   return 'Your notes...';
 }
 
-module.exports = getNotes;
+const createNote = (title, body) => {
+    console.log('Title: ' + title);
+    console.log('Body: ' + body);
+}
+
+module.exports = {getNotes, createNote};
