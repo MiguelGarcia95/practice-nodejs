3c6b27b1199f264d0301d4606427ad1f3d84d2ff
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-11 21:04:13 -0500

    parsing input

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,18 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const notes = require('./notes');
 
-const command = process.argv[2];
 
-if (command === 'add') {
-  console.log('Adding Note');
-} else if (command === 'remove') {
-  console.log('Removing Note');
-}
+// customize yargs version
+yargs.version('1.1.0');
+
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: function() {
+    console.log('Adding a new note!')
+  }
+})
+
+console.log(yargs.argv);
