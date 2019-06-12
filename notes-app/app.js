f457a475ada71d985ade5aae8afa8c4e496302fc
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-11 19:39:23 -0500

    color text in terminal

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,7 @@
-const validator = require('validator');
-
+const chalk = require('chalk');
 const notes = require('./notes');
 const note = notes();
 
 console.log(note);
 
-console.log(validator.isEmail('miguel@email.com'));
+console.log(chalk.green('Success!'));
