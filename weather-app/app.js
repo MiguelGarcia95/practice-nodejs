77805ccb78c3e61badfb83732767492b2131ea85
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-12 23:49:22 -0500

    create callback function

--- weather-app/app.js
+++ weather-app/app.js
@@ -38,6 +38,12 @@ const geocode = (address, callback) => {
       callback('Unable to connect to location services.', null);
     } else if (res.body.features.length === 0) {
       callback('Location not found... Try another search', null);
+    } else {
+      callback(null, {
+        latitude: res.body.features[0].center[1],
+        longitude: res.body.features[0].center[0],
+        location: res.body.features[0].place_name,
+      });
     }
   })
 }
