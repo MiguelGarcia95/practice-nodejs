a9b25d1a4dbd8d10c79d47dd55279bfc73ede1a9
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-12 23:59:13 -0500

    create callback function for forecast

--- weather-app/app.js
+++ weather-app/app.js
@@ -14,9 +14,30 @@ const geocode = require('./utils/geocode.js');
 //   }
 // })
 
-geocode('Houston', (err, data) => {
-  console.log('Error: ', err);
-  console.log('Data: ', data);
-})
 
 
+
+// geocode('Houston', (err, data) => {
+//   console.log('Error: ', err);
+//   console.log('Data: ', data);
+// })
+
+
+const forecast = (location, callback) => {
+  const url = `${apiData.DSKY_URL}/${apiData.DSKY_KEY}/37.8267,-122.4233?units=us&lang=en`;
+
+  request({url: url, json: true}, (err, res) => {
+    if (err) {
+      callback('Unable to connect to weather service!');
+    } else if (res.body.error) {
+      callback('Unable to find location.');
+    }
+  })
+
+}
+
+
+forecast({ latitude: 29.7589, longitude: -95.3677, location: 'houston'}, (err, data) => {
+console.log('Error: ', err);
+console.log('Data: ', data);
+});
