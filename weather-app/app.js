685c4332cbb88a674f4c867a3dd0458a50735f0a
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-12 22:23:09 -0500

    turn location to coordinates

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,15 @@
 const request = require('request');
 const apiData = require('../api_keys.js');
 
-const url = `${apiData.MAP_URL}/${apiData.DSKY_KEY}/37.8267,-122.4233?units=us&lang=en`;
+const url = `${apiData.DSKY_URL}/${apiData.DSKY_KEY}/37.8267,-122.4233?units=us&lang=en`;
 
-request({url: url, json: true}, (err, res) => {
-  console.log(`${res.body.daily.data[0].summary} It\' currently ${res.body.currently.temperature} degrees out with a ${res.body.currently.precipProbability}% chance of rain`);
+// request({url: url, json: true}, (err, res) => {
+//   console.log(`${res.body.daily.data[0].summary} It\' currently ${res.body.currently.temperature} degrees out with a ${res.body.currently.precipProbability}% chance of rain`);
+// })
+
+const geoUrl = `${apiData.MAP_URL}/Los%20Angeles.json?access_token=${apiData.MAP_KEY}&limit=1`;
+
+request({url: geoUrl, json: true}, (err , res) => {
+  console.log(`lat: ${res.body.features[0].center[0]}`);
+  console.log(`long: ${res.body.features[0].center[1]}`);
 })
