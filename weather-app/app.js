4bc9e2a405b16339d96537f3bcc4490d46015f38
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-12 23:00:06 -0500

    turn location to coordinates

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,15 +1,28 @@
 const request = require('request');
 const apiData = require('../api_keys.js');
 
-const url = `${apiData.DSKY_URL}/${apiData.DSKY_KEY}/37.8267,-122.4233?units=us&lang=en`;
+// const url = `${apiData.DSKY_URL}/${apiData.DSKY_KEY}/37.8267,-122.4233?units=us&lang=en`;
 
 // request({url: url, json: true}, (err, res) => {
-//   console.log(`${res.body.daily.data[0].summary} It\' currently ${res.body.currently.temperature} degrees out with a ${res.body.currently.precipProbability}% chance of rain`);
+//   if (err) {
+//     console.log('Unable to connect to weather service!');
+//   } else if (res.body.error) {
+//     console.log('Unable to find location');
+//   } else {
+//     console.log(`${res.body.daily.data[0].summary} It\' currently ${res.body.currently.temperature} degrees out with a ${res.body.currently.precipProbability}% chance of rain`);
+//   }
 // })
 
 const geoUrl = `${apiData.MAP_URL}/Los%20Angeles.json?access_token=${apiData.MAP_KEY}&limit=1`;
 
 request({url: geoUrl, json: true}, (err , res) => {
-  console.log(`lat: ${res.body.features[0].center[1]}`);
-  console.log(`long: ${res.body.features[0].center[0]}`);
+  if (err) {
+    console.log('Unable to connect to location map services.');
+  } else if (res.body.message) {
+    console.log('Location not found...')
+  } else {
+    console.log(`lat: ${res.body.features[0].center[1]}`);
+    console.log(`long: ${res.body.features[0].center[0]}`);
+    console.log(`place: ${res.body.features[0].place_name}`);
+  }
 })
