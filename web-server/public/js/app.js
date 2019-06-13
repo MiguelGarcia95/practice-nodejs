53fcc1300b5354cb04466b5e20bd9c7692047d16
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-13 18:04:00 -0500

    fetch http endpoint

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,17 +1,9 @@
 console.log('Client side java loaded.');
 
-// http://localhost:3000/weather?address=Houston
-
-// puzzle.mead.io/puzzle
-
-// fetch('http://puzzle.mead.io/puzzle').then(res => {
-//   return res.json()
-// }).then((data) => {
-//   console.log(data);
-// })
-
 fetch(`http://localhost:3000/weather?address=Houston`).then(res => {
   return res.json();
 }).then(data => {
   console.log(data);
+}).catch(err => {
+  console.log(err)
 })
