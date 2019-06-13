68d7763289a8f8da204da299184fabf721d7e86c
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-13 00:21:38 -0500

    create callback function for forecast

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,3 @@
-const request = require('request');
-const apiData = require('../api_keys.js');
 const geocode = require('./utils/geocode.js');
 const forecast = require('./utils/forecast.js');
 
@@ -9,7 +7,8 @@ geocode('Houston', (err, data) => {
   console.log('Data: ', data);
 })
 
-forecast({ latitude: 29.7589, longitude: -95.3677, location: 'houston'}, (err, data) => {
+// forecast({ latitude: 29.7589, longitude: -95.3677, location: 'houston'}, (err, data) => {
+forecast('29.7589', '-95.3677', (err, data) => {
   console.log('Error: ', err);
   console.log('Data: ', data);
 });
--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -1,8 +1,10 @@
 const request = require('request');
 const apiData = require('../../api_keys.js');
 
-const forecast = (location, callback) => {
-  const url = `${apiData.DSKY_URL}/${apiData.DSKY_KEY}/${location.latitude},${location.longitude}?units=us&lang=en`;
+// const forecast = (location, callback) => {
+const forecast = (latitude, longitude, callback) => {
+  // const url = `${apiData.DSKY_URL}/${apiData.DSKY_KEY}/${location.latitude},${location.longitude}?units=us&lang=en`;
+  const url = `${apiData.DSKY_URL}/${apiData.DSKY_KEY}/${latitude},${longitude}?units=us&lang=en`;
 
   request({url: url, json: true}, (err, res) => {
     if (err) {
@@ -11,11 +13,12 @@ const forecast = (location, callback) => {
       console.log(res.body.error);
       callback('Unable to find location.');
     } else {
-      callback(null, {
-        currently: res.body.currently,
-        daily: res.body.daily,
-        location: location.location
-      });
+      callback(null, `${res.body.daily.data[0].summary} It\'s currently ${res.body.currently.temperature} degrees out with a ${res.body.currently.precipProbability}% chance of rain`);
+      // callback(null, {
+      //   currently: res.body.currently,
+      //   daily: res.body.daily,
+      //   location: location.location
+      // });
     }
   })
 }
