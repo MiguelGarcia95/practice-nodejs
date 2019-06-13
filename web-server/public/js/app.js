31ab5424a3bfdd4b00454342c5b33afa3be8d2c9
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-13 18:23:16 -0500

    fetch http endpoint

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,9 +1,19 @@
 console.log('Client side java loaded.');
 
-fetch(`http://localhost:3000/weather?address=Houston`).then(res => {
-  return res.json();
-}).then(data => {
-  console.log(data);
-}).catch(err => {
-  console.log(err)
+const fetchWeather = address => {
+  if (!address) return;
+
+  fetch(`http://localhost:3000/weather?address=${address}`).then(res => {
+    return res.json();
+  }).then(data => {
+    console.log(data);
+  }).catch(err => {
+    console.log(err)
+  })
+}
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  fetchWeather(input.value)
 })
