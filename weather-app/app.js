59923635477de0892197ba12b94c0fa9fc3d79b6
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-12 21:53:05 -0500

    fetch weather data

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,7 @@
 const request = require('request');
+const apiData = require('../api_keys.js');
 
-const url = `https://api.darksky.net/forecast/a9a58e34dd722e4ef4d955bb52714d14/37.8267,-122.4233?units=us`;
+const url = `${apiData.MAP_URL}/${apiData.DSKY_KEY}/37.8267,-122.4233?units=us&lang=en`;
 
 request({url: url, json: true}, (err, res) => {
   console.log(`${res.body.daily.data[0].summary} It\' currently ${res.body.currently.temperature} degrees out with a ${res.body.currently.precipProbability}% chance of rain`);
