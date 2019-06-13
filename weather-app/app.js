570dd79d0fba1b2c1c69cabc3b02029235618f46
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-12 21:43:32 -0500

    fetch weather data

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
-const url = `https://api.darksky.net/forecast/a9a58e34dd722e4ef4d955bb52714d14/37.8267,-122.4233`;
+const url = `https://api.darksky.net/forecast/a9a58e34dd722e4ef4d955bb52714d14/37.8267,-122.4233?units=us`;
 
 request({url: url, json: true}, (err, res) => {
-  console.log(`It\' currently ${res.body.currently.temperature} degrees out with a ${res.body.currently.precipProbability}% chance of rain`);
+  console.log(`${res.body.daily.data[0].summary} It\' currently ${res.body.currently.temperature} degrees out with a ${res.body.currently.precipProbability}% chance of rain`);
 })
