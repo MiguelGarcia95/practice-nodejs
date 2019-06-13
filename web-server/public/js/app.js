49d733558c4e1447267e51400c2582a57f213b82
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-13 18:55:06 -0500

    fetch http endpoint

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,18 +1,31 @@
 console.log('Client side java loaded.');
 
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const message1 = document.getElementById('message-1');
+const message2 = document.getElementById('message-2');
+
 const fetchWeather = address => {
-  if (!address) return;
+  if (!address) {
+    return console.log('Location Input Empty');
+  };
+
+  message1.textContent = 'Loading...';
+  message2.textContent = 'Loading...';
 
   fetch(`http://localhost:3000/weather?address=${address}`).then(res => {
     return res.json();
   }).then(data => {
-    console.log(data);
+    if (data.error) throw Error(data.error);
+    message1.textContent = data.forecast;
+    message2.textContent = data.location;
   }).catch(err => {
+    message1.textContent = 'Error, Try Again...';
+    message2.textContent = `Error: ${err}`;
     console.log(err)
   })
 }
-const form = document.querySelector('form');
-const input = document.querySelector('input');
+
 form.addEventListener('submit', e => {
   e.preventDefault();
   fetchWeather(input.value)
