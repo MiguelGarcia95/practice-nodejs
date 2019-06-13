bc5cc0cd4ade90dca601a711cec096dc01c77a67
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-06-13 14:11:50 -0500

    added static files

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,16 +2,20 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicDirPath = path.join(__dirname, '../public');
+const aboutDirPath = path.join(__dirname, '../public/about');
+const helpDirPath = path.join(__dirname, '../public/help');
 
 app.use(express.static(publicDirPath));
+app.use(express.static(aboutDirPath));
+app.use(express.static(helpDirPath));
 
-app.get('/help', (req, res) => {
-  res.send({help: 'Help page, url: /help'});
-});
+// app.get('/help', (req, res) => {
+//   res.send({help: 'Help page, url: /help'});
+// });
 
-app.get('/about', (req, res) => {
-  res.send('<h1>About page, url: /about</h1>');
-});
+// app.get('/about', (req, res) => {
+//   res.send('<h1>About page, url: /about</h1>');
+// });
 
 app.get('/weather', (req, res) => {
   res.send({
